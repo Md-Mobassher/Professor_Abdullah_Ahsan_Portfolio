c206fa93513955c9b89067fc0b84897da23abf89
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-06-04 04:43:57 +0600

    first commit

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
+const NotFoundPage = () => {
+  return (
+    <div className="min-h-screen flex justify-center items-center">
+      <div className="flex flex-col justify-center items-center">
+        <h1 className="text-6xl font-bold text-red-500 text-center">404 !!!</h1>
+        <h1 className="text-7xl font-bold text-red-500  text-center mt-10">
+          Page not found!
+        </h1>
+        <Link href={`/`}>
+          <Button
+            color="primary"
+            className="hover:bg-primary hover:text-white mt-10"
+          >
+            Home Page
+          </Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default NotFoundPage;
