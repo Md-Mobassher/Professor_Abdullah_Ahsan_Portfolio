3045dcd941eae518a80088b144f29d31623717e8
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-06-04 06:57:34 +0600

    homepage updated

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -23,25 +23,25 @@ const HomePageComponent = () => {
       <div className="flex justify-center items-center mt-10 gap-10 ">
         <div className="flex-1 hidden lg:flex md:flex flex-col items-end">
           <Link
-            href="recent-update"
+            href="/books"
             className="lg:text-xl text-md font-semibold uppercase mb-3 hidden lg:flex md:flex"
           >
             Books
           </Link>
           <Link
-            href="recent-update"
+            href="/articles"
             className="lg:text-xl text-md font-semibold uppercase mb-3"
           >
             ARTICLES
           </Link>
           <Link
-            href="recent-update"
+            href="/audio-video"
             className="lg:text-xl text-md font-semibold uppercase mb-3"
           >
             AUDIO AND VIDEO
           </Link>
           <Link
-            href="recent-update"
+            href="/bios"
             className="lg:text-xl text-md font-semibold uppercase mb-3"
           >
             BIOS
@@ -50,37 +50,39 @@ const HomePageComponent = () => {
 
         <div className="flex-1">
           <div className="flex  flex-col justify-center items-center">
-            <Link
-              href="recent-update"
-              className="lg:text-xl text-md font-semibold uppercase mb-3"
-            >
-              Recent Update
-            </Link>
+            <div className="hidden lg:flex md:flex">
+              <Link
+                href="/recent-update"
+                className="lg:text-xl text-md font-semibold uppercase mb-3"
+              >
+                Recent Update
+              </Link>
+            </div>
             <Image src={ahsan} alt="Abdullah Ahsan" />
           </div>
         </div>
 
         <div className="flex-1 flex-col hidden lg:flex md:flex">
           <Link
-            href="recent-update"
+            href="/talks"
             className="lg:text-xl text-md font-semibold uppercase mb-3"
           >
             TALKS
           </Link>
           <Link
-            href="recent-update"
+            href="/debates"
             className="lg:text-xl text-md font-semibold uppercase mb-3"
           >
             DEBATES
           </Link>
           <Link
-            href="recent-update"
+            href="/letters"
             className="lg:text-xl text-md font-semibold uppercase mb-3"
           >
             LETTERS
           </Link>
           <Link
-            href="recent-update"
+            href="/about"
             className="lg:text-xl text-md font-semibold uppercase mb-3"
           >
             ABOUT
