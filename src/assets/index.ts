f0acbaca3d9e8be8e95d867811c365858bf9fe2f
Md Mobassher Hossain <mobassherpautex@gmail.com>
2024-06-26 10:34:27 +0600

    image optimized

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,19 +1,20 @@
 const assets = {
   books: {
-    oicCover: require("@/assets/books/oic.jpeg"),
-    oicBackCover: require("@/assets/books/oic-back.jpeg"),
+    oicCover: require("@/assets/books/oic.png"),
+    oicBackCover: require("@/assets/books/oic-back.png"),
+
     ummahOrNation: require("@/assets/books/ummah.png"),
+
     khalifaChildrenCover: require("@/assets/books/khilafa-cover-children.png"),
     khalifaChildrenBackCover: require("@/assets/books/khilafa-back-children.png"),
-    guidanceCover: require("@/assets/books/guidance-cover.jpg"),
-    guidanceBackCover: require("@/assets/books/guidance-back.jpg"),
+
+    guidanceCover: require("@/assets/books/guidance-cover.png"),
+    guidanceBackCover: require("@/assets/books/guidance-back.png"),
+
     quranicGuidanceCover: require("@/assets/books/quranic-guidance-cover.webp"),
     classOfCivilisationCover: require("@/assets/books/the-clash-of-civiliazation.jpg"),
     muslimFestival: require("@/assets/books/muslim-festival.jpg"),
     colonialMuslim: require("@/assets/books/Elite-formation Under Colonial Rule Capable Administrators or Loyal Servants A General Survey of Colonial Rule in Muslim Countries.jpg"),
-    // historyOfKhalifaRashida: require("@/assets/books/THE HISTORY OF AL-KHILAFAH AR-RASHIDAH.jpg"),
-
-    // secretOfLivingHappy: require("@/assets/books/secret-of-living-happy.jpg"),
   },
   icons: {
     email: require("@/assets/icons/email.png"),
